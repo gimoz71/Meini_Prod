e20c6b0b66afc4f024f9067c0f191020c4dc44d6
Gianluca Monti <gm3871@gmail.com>
2018-10-03 14:47:28 +0200

    aggunta (statica) slideshow

---
+++ config.rb
@@ -0,0 +1,29 @@
+require 'compass/import-once/activate'
+# Require any additional compass plugins here.
+
+# Set this to the root of your project when deployed:
+http_path = "/"
+# css_dir = "Assets/stylesheets"
+# sass_dir = "Assets/sass"
+# images_dir = "Assets/images"
+javascripts_dir = "Assets/javascripts"
+
+# You can select your preferred output style here (can be overridden via the command line):
+# output_style = :expanded or :nested or :compact or :compressed
+
+# To enable relative paths to assets via compass helper functions. Uncomment:
+# relative_assets = true
+
+# To disable debugging comments that display the original location of your selectors. Uncomment:
+# line_comments = false
+
+preferred_syntax = :scss
+
+css_dir = "Assets/stylesheets" # by Compass.app
+sass_dir = "Assets/sass" # by Compass.app
+images_dir = "Assets/images" # by Compass.app
+output_style = :expanded # by Compass.app
+relative_assets = true # by Compass.app
+line_comments = false # by Compass.app
+sass_options = {:debug_info=>false} # by Compass.app
+sourcemap = true # by Compass.app
